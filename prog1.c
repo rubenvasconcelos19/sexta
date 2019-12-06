4974742995f81fb069b1ed684b69bc2bae3b7d56
rubenvasconcelos19 <rubenmvasconcelos@gmail.com>
2019-12-06 09:40:52 -0100

    Prog1.c

---
+++ prog1.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+
+int main(){
+    printf("\t\t total \t\t = \t\t 100% \n \t\t IVA \t\t = \t\t 18% \n \t\t IRS \t\t = \t\t 10% \n \t\t ------------------------------------- \n \t\t Líquido \t = \t\t 68% \n");
+}
