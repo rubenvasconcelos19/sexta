111a8ba8f646201c20b1aa10421e047abd4fd53f
rubenvasconcelos19 <rubenmvasconcelos@gmail.com>
2019-12-06 09:54:45 -0100

    Sou Lindo

--- prog1.c
+++ prog1.c
@@ -2,4 +2,6 @@
 
 int main(){
     printf("\t\t total \t\t = \t\t 100% \n \t\t IVA \t\t = \t\t 18% \n \t\t IRS \t\t = \t\t 10% \n \t\t ------------------------------------- \n \t\t Líquido \t = \t\t 68% \n");
+
+    return 0;
 }
